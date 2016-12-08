2ea17ecdf7d2b8938d8cffac215d5607535cc94a
Jennifer Bramson <jib41@pitt.edu>
2016-12-08 15:57:47 -0500

    Added Ubuntu link in footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta content="autor: jenniferbramson">
     <title>1825 Days</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
   </head>
 
   <body>
